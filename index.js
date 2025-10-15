abca5650457940a7dca1981ccdda5b1721442edb
anz <g_anz@mail.ru>
2025-10-15 19:36:59 +0300

    feat: update chalk to version 5.6.2 with TypeScript definitions and improved color support
    - Added TypeScript definitions for chalk in index.d.ts
    - Implemented color support detection in index.js
    - Introduced utility functions for string manipulation in utilities.js
    - Enhanced ANSI styles with new definitions in vendor/ansi-styles
    - Improved support for color detection in vendor/supports-color
    - Added browser compatibility for supports-color
    - Updated package.json and package-lock.json for chalk dependency

---
+++ index.js
@@ -0,0 +1,88 @@
+const groups = require("./groups.json");
+
+get_work_plan(groups);
+// console.log(myData)
+async function get_work_plan(groups) {
+  for (let index = 0; index < groups.length; index++) {
+    const group = groups[index];
+    console.log(group.group_actual_name);
+    const course_number = group.group_actual_name.split(" ")[2];
+    const group_id = group.id;
+    const sub_periods = await get_sub_periods(group_id);
+    const sub_period_id = get_sub_period_id(sub_periods, course_number);
+    const work_plan = await get_work_plan(group_id, sub_period_id);
+    work_plan.forEach((plan) => {
+      console.log(`${group.group_actual_name}*${plan.name}*${plan.employers}`);
+    });
+  }
+}
+async function get_sub_periods(group_id) {
+  const response = await fetch(
+    "https://college.07.edu.o7.com/actions/group_subperiod/objectrowsactionforgroup",
+    {
+      headers: {
+        accept: "*/*",
+        "accept-language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua":
+          '"Microsoft Edge";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "x-xsrftoken":
+          "cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+        cookie:
+          "_ym_uid=1738241477713532996; _ym_d=1738241477; csrf_token_header_name=X-XSRFTOKEN; userNotifiedAboutCookieUsage=t; ssuz_sessionid=jv2ftt1dqvi3u1uhkb28k96a5c0vj04l; csrftoken=cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+        Referer: "https://college.07.edu.o7.com/desk/",
+      },
+      body: `start=0&limit=50&m3_window_id=cmp_3801a17e&groups_id=${group_id}`,
+      method: "POST",
+    }
+  );
+
+  const suberiods = await response.json();
+  return suberiods;
+}
+
+function get_sub_period_id(sub_periods, course_number) {
+  const sub_period = sub_periods.rows.filter(
+    (sub_period) =>
+      sub_period.period__grouplevel__course == course_number &&
+      sub_period.phase_id == 8
+  );
+
+  return sub_period[0].id;
+}
+
+async function get_work_plan(group_id, sub_period_id) {
+  const response = await fetch(
+    "https://college.07.edu.o7.com/actions/work_plan/work_plan_rows",
+    {
+      headers: {
+        accept: "*/*",
+        "accept-language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua":
+          '"Microsoft Edge";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "x-xsrftoken":
+          "cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+        cookie:
+          "_ym_uid=1738241477713532996; _ym_d=1738241477; csrf_token_header_name=X-XSRFTOKEN; userNotifiedAboutCookieUsage=t; ssuz_sessionid=jv2ftt1dqvi3u1uhkb28k96a5c0vj04l; csrftoken=cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+        Referer: "https://college.07.edu.o7.com/desk/",
+      },
+      body: `group_id=${group_id}&subperiod_id=${sub_period_id}`,
+      method: "POST",
+    }
+  );
+  const work_plan = await response.json();
+  return work_plan.rows;
+}
--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 import sys
-import json
 import requests
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-try:
-    with open('groups.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    # for item in data:
-    #     print(item)
-    print(data[0])
-    headers = {
-        "accept": "*/*",
-        "accept-language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "sec-ch-ua": "\"Microsoft Edge\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-requested-with": "XMLHttpRequest",
-        "x-xsrftoken": "1eOupmVLuRm84jII0R9YY6F9K0EmzUpxGqrGOiDWIIr8VYS09rshzlVREJ8bOIM8",
-        "cookie": "_ym_uid=1738241477713532996; _ym_d=1738241477; csrf_token_header_name=X-XSRFTOKEN; userNotifiedAboutCookieUsage=t; ssuz_sessionid=pl6gxnqmpalldbru200yxwpqv67s0jxn; csrftoken=1eOupmVLuRm84jII0R9YY6F9K0EmzUpxGqrGOiDWIIr8VYS09rshzlVREJ8bOIM8",
-        "Referer": "https://college.07.edu.o7.com/desk/"
-    }
-    body = {"start": 0, "limit": 50, "groups_id": 2758}
 
-    r = requests.post(
-        'https://college.07.edu.o7.com/actions/group_subperiod/objectrowsactionforgroup', headers=headers, json=data)
-    print(r.text)
+url = "https://college.07.edu.o7.com/actions/group_subperiod/objectrowsactionforgroup"
 
-    # print(f"Parsed data: {data}")
+headers = {
+    "accept": "*/*",
+    "accept-language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "x-requested-with": "XMLHttpRequest",
+    "x-xsrftoken": "cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+    "cookie": "_ym_uid=1738241477713532996; _ym_d=1738241477; csrf_token_header_name=X-XSRFTOKEN; "
+              "userNotifiedAboutCookieUsage=t; ssuz_sessionid=jv2ftt1dqvi3u1uhkb28k96a5c0vj04l; "
+              "csrftoken=cBD1E3Xidgjx621rpuPe4g5afRtrEvvyzTlvhBGqNv8VbtqHBRoEuZW9RIAWaWkO",
+    "Referer": "https://college.07.edu.o7.com/desk/",
+}
 
-except FileNotFoundError:
-    print("Error: 'data.json' not found.")
-except json.JSONDecodeError:
-    print("Error: Failed to decode JSON from the file. Check for malformed JSON.")
+data = {
+    "start": "0",
+    "limit": "50",
+    "m3_window_id": "cmp_3801a17e",
+    "groups_id": "2606"
+}
+
+response = requests.post(url, headers=headers, data=data)
+response.raise_for_status()
+print(response.json())
